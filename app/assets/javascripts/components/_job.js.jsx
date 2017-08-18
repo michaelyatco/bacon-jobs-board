c5180ca19de9fc58c368230051215a9fe899c1cc
michaelyatco <mjyatco@gmail.com>
2017-08-18 16:10:10 -0400

    Editing dynamic

--- app/assets/javascripts/components/_job.js.jsx
+++ app/assets/javascripts/components/_job.js.jsx
@@ -8,12 +8,23 @@ var Job = React.createClass({
   },
 
   render() {
+    var title = this.state.editable ? <input type='text' defaultValue={this.props.job.title} /> : <h3>{this.props.job.title} </h3>
+    var company = this.state.editable ? <input type='text' defaultValue={this.props.job.company} /> : <p>{this.props.job.company} </p>
+    var location = this.state.editable ? <input type='text' defaultValue={this.props.job.location} /> : <p>{this.props.job.location} </p>
+    var description = this.state.editable ? <input type='text' defaultValue={this.props.job.description} /> : <p>{this.props.job.description} </p>
+    var jobtype= this.state.editable ? <input type='text' defaultValue={this.props.job.jobtype} /> : <p>{this.props.job.jobtype} </p>
+    var price = this.state.editable ? <input type='text' defaultValue={this.props.job.price} /> : <p>{this.props.job.price} </p>
+
     return (
       <div>
-        <h3>{this.props.job.name}</h3>
-        <p>{this.props.job.description}</p>
+        {title}
+        {company}
+        {location}
+        {description}
+        {jobtype}
+        {price}
         <button onClick={this.props.handleDelete} > Delete </button>
-        <button onClick={this.props.handleEdit} > Edit </button>
+        <button> {this.state.editable ? 'Submit' : 'Edit' } </button>
       </div>
     )
   }
