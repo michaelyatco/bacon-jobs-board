1242f4bb1e4475f66c8945c662e88e415dd4fb89
michaelyatco <mjyatco@gmail.com>
2017-08-18 14:05:12 -0400

    API base and jobs controller created with appropriate routes for rendering pure JSON

---
+++ app/controllers/api/v1/jobs_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::JobsController < Api::V1::BaseController
+  def index
+    respond_with Job.all
+  end
+
+  def create
+    respond_with :api, :v1, Job.create(job_params)
+  end
+
+  def destroy
+    respond_with Job.destroy(params[:id])
+  end
+
+  def update
+    job = Job.find(params["id"])
+    job.update_attributes(job_params)
+    respond_with job, json: job
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:id, :title, :company, :location, :description, :jobtype, :price)
+  end
+
+end
