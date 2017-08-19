df37a0027caee2b8b2d98dc15258a959e5003b23
michaelyatco <mjyatco@gmail.com>
2017-08-19 12:05:09 -0400

    disabled price parameter

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,11 +1,11 @@
 class Job < ApplicationRecord
-  validates :title, :company, :location, :description, :jobtype, :price, presence: true
-  validates :price, numericality: true
+  validates :title, :company, :location, :description, :jobtype, presence: true
+  # validates :price, numericality: true
   validates :description, length: { maximum: 1000 }
-  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0}
+  # validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0}
 
-  def price
-   "$%.2f" % self[:price]
-  end
+  # def price
+  #  "$%.2f" % self[:price]
+  # end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170818174603) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.string "company"
